0740c752431005dde9b7e311458989aa43afa40a
Suleman <aseem.a.suleman@capgemini.com>
2022-07-29 12:18:22 +0100

    updated keyvault

--- Modules/resources/key_vault/resource_key_vault.tf
+++ Modules/resources/key_vault/resource_key_vault.tf
@@ -36,7 +36,7 @@ resource "azurerm_key_vault" "example_key_vault" {
       "Get",
     ] */
   }
-}
+
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
@@ -60,7 +60,7 @@ resource "azurerm_key_vault_secret" "example" {
   count = var.resource_instance_count
 
   #name                    = "${var.azure_key_vault}-${var.unique_ID}"
-  name                    = "${var.azure_key_vault}-kv"
+  name                    = "${var.azure_key_vault}-kv ${count.index +1}"
   value                   = var.key_vault_password[count.index].result
   key_vault_id            = azurerm_key_vault.example_key_vault.id
 }
